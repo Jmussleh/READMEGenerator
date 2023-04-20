287bb66c93cb482678efe014b65526eba575ba2c
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-04-20 12:56:53 -0400

    Added the questions for the inquirer in the index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,65 @@
 // TODO: Include packages needed for this application
-
+var inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+      
+      {
+        type: 'input',
+        message: 'Write a short description of your project...',
+        name: 'description',
+      },
+    
+      {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'install',
+      },
+    
+      {
+            type: 'input',
+            message: 'What should the user use this application for?',
+            name: 'usage',
+            default: 'To dynamically generate a professional README.md file.'
+      },
+
+      {
+        type: 'list',
+        message: 'What license was used for this project?',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'Mozilla 2.0', 'GNU GPLv3', 'None' ],
+        default: 'None',
+      },
+    
+      {
+        type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'contribute',
+      },  
+
+      {
+        type: 'input',
+        message: 'What testing was done for this project?',
+        name: 'test',
+      },
+
+      {
+        type: 'input',
+        message: 'Enter your GitHub username.',
+        name:'github',
+      },
+
+      {
+        type: 'input',
+        message: 'Enter your email.',
+        name: 'email',
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
