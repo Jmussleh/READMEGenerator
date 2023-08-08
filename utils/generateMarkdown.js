8ade1c02b8453d4bd42bf92d5eb134d765f525ea
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-08-08 08:34:22 -0400

    Fixed small things in table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,11 +33,9 @@ function generateMarkdown(data) {
   ${renderLicenseBadge(data.license)}
 
   ## Description
-  
   ${data.description}
   
   ## Table of Contents 
-  
   * [Installation](#installation)
   
   * [Usage](#usage)
@@ -49,33 +47,25 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   
   ## Installation
-  
   To install necessary dependencies, run this command:
-  
   \`\`\`
   ${data.install}
   \`\`\`
   
   ## Usage
-  
   ${data.usage}
-  
   ${renderLicenseSection(data.license)}
     
   ## Contributing
-  
   ${data.contribute}
   
   ## Tests
-  
   To run tests, run this command:
-  
   \`\`\`
   ${data.test}
   \`\`\`
   
   ## Questions
-  
   If you have any questions about the repo, open an issue or contact me directly at ${
       data.email
     }. You can find more of my work at [${data.github}](https://github.com/${
